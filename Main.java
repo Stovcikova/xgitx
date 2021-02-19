92ce4eb0b0f041d1bc278759450e341a6b041d09
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 17:32:59 +0100

    Update Main.java
    changed

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
 class Main {
+    // hlavna metoda
     public static void main(String[] args) {
+        // premenne
         String str1 = "Java";
-        String str2 = "";
+        String str2 = "Basic 6";
 
         System.out.println(str1.length());  
         System.out.println(str2.length());  
-        System.out.println("Java".length());  
+        System.out.println("Java".trim().length());  
+        
         System.out.println("Java\n".length()); 
         System.out.println("Learn Java".length()); 
+        /* dfdf */
+    }
+    public int getValue(){
+        return 5;
     }
 }
