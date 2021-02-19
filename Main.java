5d7d35e80fe4ce85efd6ce5c8db49c4566e0f1a9
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 17:31:40 +0100

    Update Main.java
    changed

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 class Main {
     public static void main(String[] args) {
-        String str1 = "Java";
-        String str2 = "";
-
+        // comment
+        String str1 = "Java 16";
+        String str2 = "Visual Basic";
+        
         System.out.println(str1.length());  
         System.out.println(str2.length());  
+        
         System.out.println("Java".length());  
         System.out.println("Java\n".length()); 
         System.out.println("Learn Java".length()); 
+    }
+    public void hello(){
+        // tu nieco bude
     }
 }
